b5a4600c306f9584d9d9c7714d61fd4095a50694
asimplepenguin <chimbetetemukundi@gmail.com>
2024-10-26 20:57:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss"
+
+import Logo from "./assets/react.svg"
+function App() {
+  return(
+   <>
+   <div className="Card">
+   <h4>I love Python</h4> 
+   <p>I dont like pears because the skin taste like  dirt</p>
+   <button>Press me </button>
+   <a href="https://www.youtube.com">open youtube</a>
+   </div>
+  
+   </>
+
+  )  
+}
+export default App ;
